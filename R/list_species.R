313e398477e5750024c878a8abb2666761de92a0
Walid Korani <waleedkorany@yahoo.com>
2017-04-28 14:39:32 -0600

    Update list_species.R

--- R/list_species.R
+++ R/list_species.R
@@ -1,14 +1,14 @@
 #'name list_species 
 list_species <- function(key_word)
 {
-	Link1 = "http://rest.kegg.jp/list/organism"
-	Table1 = readLines(Link1)
-	Table2 = t(data.frame(strsplit(Table1,'\t')))
-	Table3 = cbind(organism_abb = as.character(Table2[,2]),description = as.character(Table2[,3]))
-	if (missing(key_word))
-	{return(Table3)}
-	else
-	{
-	return(Table3[grep(key_word,Table3[,2],ignore.case=TRUE),])
-	}
+    Link1 = "http://rest.kegg.jp/list/organism"
+    Table1 = readLines(Link1)
+    Table2 = t(data.frame(strsplit(Table1,'\t')))
+    Table3 = cbind(organism_abb = as.character(Table2[,2]),description = as.character(Table2[,3]))
+    if (missing(key_word))
+    {return(Table3)}
+    else
+    {
+    return(Table3[grep(key_word,Table3[,2],ignore.case=TRUE),])
+    }
 }
