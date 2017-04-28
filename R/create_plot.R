21eddd3fd2b6f2d4efbfa4b682271ffc3d3b0127
Walid Korani <waleedkorany@yahoo.com>
2017-04-28 16:10:21 -0600

    Update create_plot.R

--- R/create_plot.R
+++ R/create_plot.R
@@ -9,13 +9,15 @@ create_plot <- function(dataset)
     Link1 = "http://rest.kegg.jp/list/pathway"
     Table1 = readLines(Link1)
     Table2 = t(data.frame(strsplit(Table1,'\t')))
-    Table3 = cbind(map_numbers = as.character(Table2[,1]),pathway_description = as.character(Table2[,2]))
+    Table3 = cbind(map_numbers = as.character(Table2[,1]),
+    pathway_description = as.character(Table2[,2]))
     Table4 = gsub('path:map','',Table3)
     Table5 = data.frame(Table4)
     Table6 = merge(Table5,Results,by="map_numbers")
     Rich_factor = Table6$deg_gene_numbers/Table6$background_gene_numbers
     Rich_factor = round(Rich_factor ,digits=2)
-    p <- ggplot2::ggplot(Table6, ggplot2::aes(x = Rich_factor, y = pathway_description))
+    p <- ggplot2::ggplot(Table6, ggplot2::aes(x = Rich_factor, 
+    y = pathway_description))
     p + ggplot2::geom_point(ggplot2::aes(
     colour = qvalue,size = Table6$deg_gene_numbers))+ 
     ggplot2::scale_colour_gradientn(colours=c("blue","white","red"))
