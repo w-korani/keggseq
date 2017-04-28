cdf06a6d2cea6276980e1728e3c8f88b911b6dd1
Walid Korani <waleedkorany@yahoo.com>
2017-04-28 14:30:54 -0600

    Update corrected_datasets_ids.R

--- R/corrected_datasets_ids.R
+++ R/corrected_datasets_ids.R
@@ -1,11 +1,11 @@
 #'name corrected_datasets_ids
 corrected_datasets_ids <- function(dataset_orginal,dataset_alternate_file)
 {
-	dataset_alternate = read.csv(dataset_alternate_file,header=FALSE)
-	colnames(dataset_alternate) = c("kegg_genes_new","kegg_genes")
-	Table4b <- merge(dataset_alternate,dataset_orginal,by = "kegg_genes")
-	Table4c <- Table4b[,c(2,3)]
-	colnames(Table4c) = c("kegg_genes","kegg_pathways")
-	Table4c = Table4c[order(Table4c$kegg_pathways),]
-	return(Table4c)
+    dataset_alternate = read.csv(dataset_alternate_file,header=FALSE)
+    colnames(dataset_alternate) = c("kegg_genes_new","kegg_genes")
+    Table4b <- merge(dataset_alternate,dataset_orginal,by = "kegg_genes")
+    Table4c <- Table4b[,c(2,3)]
+    colnames(Table4c) = c("kegg_genes","kegg_pathways")
+    Table4c = Table4c[order(Table4c$kegg_pathways),]
+    return(Table4c)
 }
