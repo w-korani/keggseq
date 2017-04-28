b2564c2a7d05a85a5d543a4eabf8f669c7bb24d1
Walid Korani <waleedkorany@yahoo.com>
2017-04-28 16:13:13 -0600

    Update list_species.R

--- R/list_species.R
+++ R/list_species.R
@@ -4,7 +4,8 @@ list_species <- function(key_word)
     Link1 = "http://rest.kegg.jp/list/organism"
     Table1 = readLines(Link1)
     Table2 = t(data.frame(strsplit(Table1,'\t')))
-    Table3 = cbind(organism_abb = as.character(Table2[,2]),description = as.character(Table2[,3]))
+    Table3 = cbind(organism_abb = as.character(Table2[,2]),
+    description = as.character(Table2[,3]))
     if (missing(key_word))
     {return(Table3)}
     else
