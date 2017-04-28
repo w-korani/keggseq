37ae8b3fc8ae2f3ddc3e5d97ae7a8b31aead731a
Walid Korani <waleedkorany@yahoo.com>
2017-04-28 12:05:09 -0600

    Update corrected_datasets_ids.R

--- R/corrected_datasets_ids.R
+++ R/corrected_datasets_ids.R
@@ -1,7 +1,7 @@
 #'name corrected_datasets_ids
 corrected_datasets_ids <- function(dataset_orginal,dataset_alternate_file)
 {
-	dataset_alternate = read.csv(dataset_alternate_file,header=F)
+	dataset_alternate = read.csv(dataset_alternate_file,header=FALSE)
 	colnames(dataset_alternate) = c("kegg_genes_new","kegg_genes")
 	Table4b <- merge(dataset_alternate,dataset_orginal,by = "kegg_genes")
 	Table4c <- Table4b[,c(2,3)]
